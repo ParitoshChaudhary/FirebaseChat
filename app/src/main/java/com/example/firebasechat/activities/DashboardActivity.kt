9b652a863bcaf3f7c2ea55d8dc480219d1f1d717
Paritosh Chaudhary <paritoshc.at@gmail.com>
2020-10-03 13:38:13 +0530

    Added Settings page layout.

--- app/src/main/java/com/example/firebasechat/activities/DashboardActivity.kt
+++ app/src/main/java/com/example/firebasechat/activities/DashboardActivity.kt
@@ -38,6 +38,7 @@ class DashboardActivity : AppCompatActivity() {
         return true
     }
 
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         super.onOptionsItemSelected(item)
 
